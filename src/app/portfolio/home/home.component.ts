9afc94a07f9485a52645f9900322fd5e35955be1
Dielli Baldedaj <112899402+DielliXD@users.noreply.github.com>
2024-04-12 18:06:34 +0200

    Update home.component.ts

--- src/app/portfolio/home/home.component.ts
+++ src/app/portfolio/home/home.component.ts
@@ -11,7 +11,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // Run GSAP animation when the component is initialized
     gsap.from('.container', { opacity: 0, duration: 1, delay: 0.5 });
     gsap.from('.photo', { opacity: 0, y: -100, rotation: -45, scale: 0.5, duration: 1.2, ease: 'back.out(1.7)', delay: 0.7 });
     gsap.from('.text', { opacity: 0, x: 100, duration: 1, delay: 1 });
